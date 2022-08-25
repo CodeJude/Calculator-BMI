aa987e9b90b5ef3f3aec580e255000608e8fe695
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2022-08-25 17:23:53 +0400

    initial commit

---
+++ calculator.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+const port = 3030
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post('/', function(req, res){
+
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+
+    let result = num1 + num2;
+
+    res.send(`The result of the calculation is ${result}`)
+})
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post('/bmicalculator', function(req, res){
+
+    let weight = parseFloat(req.body.weight);
+    let height = parseFloat(req.body.height);
+    
+    let bmi = weight / Math.pow(height, 2);
+
+    res.send(`The BMI is : ${bmi}`);
+})
+
+
+
+app.listen(port, function(){
+    console.log(`Server is running on port ${port}`);
+});
